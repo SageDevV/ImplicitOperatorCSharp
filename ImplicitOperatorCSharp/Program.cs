a7bca624b324f11b597c2bb8d3b0d381e9dedad9
SageDevV <pandredbz@gmail.com>
2022-07-20 14:52:06 -0300

    first commit

---
+++ ImplicitOperatorCSharp/Program.cs
@@ -0,0 +1,29 @@
+﻿using ImplicitOperatorCSharp;
+
+//Conversão implicita de string para objeto complexo HappyWay
+string telefone = "55 47 992843977";
+string telefoneErrado = "Teste";
+var telefoneObject = new Telefone();
+telefoneObject = telefone;
+
+Console.WriteLine(telefoneObject);
+
+//Conversão implicita de string para objeto complexo BadWay
+telefoneObject = telefoneErrado;
+
+Console.WriteLine(telefoneObject);
+
+//Conversão implicita de objeto complexo para string 
+var telefoneObject2 = new Telefone()
+{
+    PrefixoPais = "55",
+    DDD = "47",
+    Número = "992843977"
+};
+string telefone2 = telefoneObject2;
+
+Console.WriteLine(telefone2);
+
+
+
+
---
+++ ImplicitOperatorCSharp/Telefone.cs
@@ -0,0 +1,36 @@
+﻿namespace ImplicitOperatorCSharp
+{
+    public class Telefone
+    {
+        public string PrefixoPais { get; set; }
+        public string DDD { get; set; }
+        public string Número { get; set; }
+
+        public static implicit operator Telefone (string telefone)
+        {
+            try
+            {
+                var data = telefone.Split(" ");
+                return new Telefone
+                {
+                    PrefixoPais = data[0],
+                    DDD = data[1],
+                    Número = data[2]
+                };
+            }
+            catch (Exception)
+            {
+                throw new InvalidCastException("Conversão implicita inválida");
+            }
+            
+        }
+
+        public static implicit operator string (Telefone telefone)
+            => $"{telefone.PrefixoPais} {telefone.DDD} {telefone.Número}";
+    }
+}
+
+
+
+
+
